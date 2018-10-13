3258768ad7cb7e66cd40290165e450dca81bc140
Luca Napoletano <10464485@polimi.it>
2018-10-13 20:51:40 +0200

    Modified version of build inside build_data

--- src/build_data.py
+++ src/build_data.py
@@ -1,22 +1,15 @@
-import pandas as p
-import scipy as s
+import scipy.sparse as sps
+import numpy as np
+import scipy as sp
 
-import data
+from src import data
 
 
 def build():
-    train_df = p.read_csv('../data/train.csv')
-    train_df.head()
-    train_df['rate'] = 1
-    URM = train_df.pivot(index="playlist_id", columns="track_id", values="rate").fillna(0).to_sparse(fill_value=0)
-    sparseURM = URM.to_sparse()
-    cooURM = URM.to_coo()
-    dm = data.Data_manager(cooURM);
+    datafile = np.loadtxt('../input/train.csv', delimiter=',', skiprows=1)
+    userList, itemList = zip(*datafile)
+    ratings = np.ones(1211791)
+    URM_all = sps.coo_matrix((ratings, (userList, itemList)))
+    dm = data.Data_manager(URM_all)
     return  dm
-
-
-def loadTarget():
-    t = p.DataFrame.from_csv('../data/target_playlists.csv')
-    return t.values
-
 
